f6e37781046f6556fcb6a170cf4e49006124182a
jontrev20 <78224683+jontrev20@users.noreply.github.com>
2021-03-14 20:08:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randNumber = Math.floor(Math.random()*20);
+var randNumber = Math.floor(Math.random()*19);
 var responseList = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definately",
 "You may rely on it.", "As I see it, yes.", "Most likely", "Outlook good.", "Yes.",,
 "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
